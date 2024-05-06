860cb033bd2ab4b33e547e8ddd63de84819c8907
Auto Logon <autologon@CSED.LAB>
2024-05-06 11:14:23 -0300

    Commit anterior errado, algumas alterações feitas

--- Academia/De/Trabalho/Classes/Exercicio.java
+++ Academia/De/Trabalho/Classes/Exercicio.java
@@ -3,5 +3,34 @@ package Academia.De.Trabalho.Classes;
 public class Exercicio {
     private String nome;
     private Equipamento equipamento;
+
+
+    public Exercicio(String nome, Equipamento equipamento) {
+        this.nome = nome;
+        this.equipamento = equipamento;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public Equipamento getEquipamento() {
+        return equipamento;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public void setEquipamento(Equipamento equipamento) {
+        this.equipamento = equipamento;
+    }
+
+    
+    
     
 }
